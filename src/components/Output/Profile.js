3b3b3c8a0ca301db405dc4b011079bc24cd7108f
Aiza Ali <aiza9382@gmail.com>
2024-01-26 18:06:05 +0500

    profile output section updated

--- src/components/Output/Profile.js
+++ src/components/Output/Profile.js
@@ -1,22 +1,26 @@
-"use client"
+"use client";
 
 import PersonalStatesContext from "@/Context/PersonalContext";
 import { parseContent } from "@/utils";
-import { useContext } from "react";
-import classes from "./Profile.module.css"
+import { useContext, useEffect, useState } from "react";
+import classes from "./Profile.module.css";
 
 const Profile = () => {
-  const { profileDescription } = useContext(PersonalStatesContext)
-    const desc = parseContent(profileDescription)
-    return (
-        <div className={`mb-6 ${profileDescription ? "block" : "hidden"}`}>
-        <span className="h-style">Profile</span>
-        <div
-          className={classes.desc}
-          dangerouslySetInnerHTML={{ __html: desc }}
-        />
-      </div>
-    )
+  const { profileDescription } = useContext(PersonalStatesContext);
+  const desc = parseContent(profileDescription);
+  return (
+    <>
+      {profileDescription && (
+        <div className={`mb-6 `}>
+          <span className="h-style">Profile</span>
+          <div
+            className={classes.desc}
+            dangerouslySetInnerHTML={{ __html: desc }}
+          />
+        </div>
+      )}
+    </>
+  );
 };
 
 export default Profile;
