b31f132da1113141afca1ee9e53231e19c8936a2
Aiza Ali <aiza9382@gmail.com>
2024-01-26 17:35:28 +0500

    skills output section updated

--- src/components/Output/Skill.js
+++ src/components/Output/Skill.js
@@ -2,23 +2,36 @@
 
 import SkillContext from "@/Context/SkillContext";
 import { CategoryIcon } from "@/Icons/OutputIcons";
-import { useContext } from "react";
-import classes from "./Skill.module.css"
+import { useContext, useEffect, useState } from "react";
+import classes from "./Skill.module.css";
 
 const Skills = () => {
   const { categories } = useContext(SkillContext);
+  const [isHidden, setIsHidden] = useState(false)
+
+  useEffect(() => {
+    const skills = categories && categories.some(cate =>
+      cate.skills && cate.skills.length > 0
+    );
+    const cateName =  categories && categories.some(cate =>
+      cate.name && cate.name.trim() !== "")
+
+      setIsHidden(!(skills || cateName))
+
+  }, [categories])
   return (
-    <div className="pl-6">
+    <div className={`pl-6 ${isHidden ? "hidden" : "" }`}>
       <span className="h-style">Skills</span>
       {categories &&
         categories?.map((cate) => (
           <>
-            <div className={`flex-items ${classes["cate-name_wrapper"]}`}>
-              <CategoryIcon />
-              <span className={classes["cate-name"]}>
-                {cate?.name}
-              </span>
-            </div>
+            {cate.name && (
+              <div className={`flex-items ${classes["cate-name_wrapper"]}`}>
+                <CategoryIcon />
+                <span className={classes["cate-name"]}>{cate?.name}</span>
+              </div>
+            )}
+
             <div className={classes.skills_wrapper}>
               {cate.skills &&
                 cate.skills.map((skill) => (
